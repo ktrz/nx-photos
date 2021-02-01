13390a05f90cd5dd30bae49570e2445392cf296e
Chris Trześniewski <k.trzesniewski@gmail.com>
2021-02-01 08:59:42 +0100

    add ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {PhotoModule} from './photo/photo.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {PhotoModule} from './photo/photo.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    PhotoModule
+    PhotoModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
