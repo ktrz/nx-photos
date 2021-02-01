4528716eeecbd22f395f393c181020703f2ae8a1
Chris Trześniewski <k.trzesniewski@gmail.com>
2021-02-01 09:07:37 +0100

    add basic actions

---
+++ src/app/store/actions.ts
@@ -0,0 +1,4 @@
+import {createAction} from '@ngrx/store';
+
+export const likePhoto = createAction('[Photo List] Like Photo');
+export const dislikePhoto = createAction('[Photo List] Dislike Photo');
