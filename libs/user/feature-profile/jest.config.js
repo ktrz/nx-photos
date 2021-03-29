ec51e4b3a0d978b840f0b3ac090399129e8f9ece
Chris Trześniewski <k.trzesniewski@gmail.com>
2021-03-29 09:50:20 +0200

    rename library

--- libs/user/feature-profile/jest.config.js
+++ libs/user/feature-profile/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'profile-feature-profile',
+  displayName: 'user-feature-profile',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  coverageDirectory: '../../../coverage/libs/profile/feature-profile',
+  coverageDirectory: '../../../coverage/libs/user/feature-profile',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
--- libs/user/feature-profile/src/lib/user-feature-profile.module.ts
+++ libs/user/feature-profile/src/lib/user-feature-profile.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-export class ProfileFeatureProfileModule {}
+export class UserFeatureProfileModule {}
